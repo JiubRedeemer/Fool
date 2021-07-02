d345310ff09538a36b91902da6c0e1e5b565da8a
JiubRedeemer <jiubredeemer@gmail.com>
2021-07-02 10:48:31 +0400

    Load
    Load files

---
+++ Assets/Scritpts/Character.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    protected float speed;
+    protected float rotationAngle;
+
+    protected Rigidbody2D rb;
+    protected SpriteRenderer sprite;
+
+    protected Vector2 movement;
+    protected Vector2 lookDir;
+
+    
+
+    protected virtual void Move() { }
+
+
+    protected virtual void Rotate() { }
+   
+
+
+}
---
+++ Assets/Scritpts/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public Camera cam;
+    private Vector3 faceDir;
+    Vector3 mousePos;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        speed = 2.0F;
+    }
+
+    void Update()
+    {
+        Move();
+        faceDir = Input.mousePosition;
+        mousePos = cam.ScreenToWorldPoint(faceDir);
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        
+
+        Vector2 lookDir = mousePos - transform.position;
+        Debug.Log(lookDir);
+
+
+        Debug.DrawRay(transform.position, lookDir, Color.yellow);
+
+        rotationAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        rb.rotation = rotationAngle;
+        
+    }
+    protected override void Move()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    protected override void Rotate()
+    {
+        
+    }
+}
