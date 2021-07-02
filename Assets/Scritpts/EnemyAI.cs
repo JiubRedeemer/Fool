6bf0991b8cf34a393aa823c733de7216b430325e
JiubRedeemer <jiubredeemer@gmail.com>
2021-07-02 13:38:02 +0400

    Add EnemyAI viewing and hearing

--- Assets/Scritpts/EnemyAI.cs
+++ Assets/Scritpts/EnemyAI.cs
@@ -10,33 +10,51 @@ public class EnemyAI : MonoBehaviour
 
     private float speed;
     private float hearArea = 3.0F;
+    private float veawArea = 6.0F;
 
     private Rigidbody2D rb;
+    private Player player;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
+        player = FindObjectOfType<Player>();
+            }
 
     private void Update()
     {
-        listening();
+       // listening();
+        viewing();
     }
-    private void listening() {
+    private void listening()
+    {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, hearArea);
-        for (int i = 0; i < colliders.Length; i++) {
-            if (colliders[i].tag == "Player") {
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].tag == "Player")
+            {
                 Debug.Log("Catch"); //TODO Logic danger
                 Transform victim = colliders[i].transform;
                 Debug.Log(victim.position);
 
-            } 
+            }
 
-        }
-
-
-        //Debug.Log(colliders[colliders.Length-1].tag);
-
-        private void viewing()
-        {
         }
     }
+
+    //Debug.Log(colliders[colliders.Length-1].tag);
+    private bool detectWall = false;
+    private void viewing()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, player.transform.position, veawArea);
+        for (int i = 0; i < hits.Length; i++) {
+            Debug.Log(hits[i].collider.tag);
+            
+            if (hits[i].collider.tag == "Wall") detectWall = true;
+        }
+        Debug.Log(hits.Length);
+        if (detectWall == false) Debug.Log("See this asshole!");
+        detectWall = false;
+        Debug.DrawRay(transform.position, player.transform.position, Color.red);
+
+    }
+}
