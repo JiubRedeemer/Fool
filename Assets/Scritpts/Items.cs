bcd597b4d36861f3a2d376f0ef77e88d22678ea4
JiubRedeemer <jiubredeemer@gmail.com>
2021-07-06 16:53:15 +0400

    Add Inventory(not correct now)

--- Assets/Scritpts/Items.cs
+++ Assets/Scritpts/Items.cs
@@ -5,33 +5,40 @@ using UnityEngine;
 public class Items : MonoBehaviour
 {
     PlayerStats playerStats;
-  
+    public Inventory inventory;
 
+    //Water
+    public Sprite waterSprite;
     string waterTag = "Water";
+    private int waterId = 1;
     private float waterStamina = 20.0F;
     private float waterPeeLvl = 10.0F;
     void Start()
     {
         playerStats = FindObjectOfType<PlayerStats>();
-        
-        
+        inventory = FindObjectOfType<Inventory>();   
     }
 
     void Update()
     {
-        Water();
+        TakeWater();
     }
-    public void Water() {
+    public void TakeWater() {
         Collider2D[] cols = Physics2D.OverlapCircleAll(playerStats.player.transform.position, 0.5F);
         for (int i = 0; i < cols.Length; i++)
         {
-            if (cols[i].tag == waterTag) { 
-                playerStats.Stamina += waterStamina;
-                playerStats.PeeLvl += waterPeeLvl;
-                playerStats.setNormalSpeed();
+            if (cols[i].tag == waterTag) {
+                inventory.AddItem(waterId, waterSprite);
+                
                 Destroy(cols[i].gameObject);
                 
             }
         }
+    }
+
+    public void UseWater() {
+        playerStats.Stamina += waterStamina;
+        playerStats.PeeLvl += waterPeeLvl;
+        playerStats.setNormalSpeed();
     }
 }
