f28e0034fda6321447a5e19c1a73c90c9edfa2c6
bitbondy <bitbondy@yandex.ru>
2021-07-13 18:51:20 +0300

    first load
    Floor4/3

---
+++ Assets/Scritpts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public bool pushed = false;
+    private void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick() {
+        pushed = true;
+    }
+}
